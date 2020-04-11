c78472d0105ff74425696373f0fe6566da3fc98c
Oguz Tandogan <oguz.tandogan@gmail.com>
2020-04-11 22:19:25 +0300

    Initial Commit

---
+++ Chatty/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chatty
+//
+//  Created by Oguz Tandogan on 11.04.2020.
+//  Copyright © 2020 Oguz Tandogan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ChattyTests/ChattyTests.swift
@@ -0,0 +1,34 @@
+//
+//  ChattyTests.swift
+//  ChattyTests
+//
+//  Created by Oguz Tandogan on 11.04.2020.
+//  Copyright © 2020 Oguz Tandogan. All rights reserved.
+//
+
+import XCTest
+@testable import Chatty
+
+class ChattyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
